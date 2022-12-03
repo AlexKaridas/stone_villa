6f86a1a3b7d4bf1f18e5da0f739438c5a57c9c55
Alex Karidas <72619747+AlexKaridas@users.noreply.github.com>
2022-12-03 13:59:32 +0200

    2.4

--- pages/_document.js
+++ pages/_document.js
@@ -25,8 +25,9 @@ export default function Document() {
                     crossOrigin
                 /> */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" ></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin ></link>
-                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin ></link>
+                {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link> */}
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
                 <script src="https://unpkg.com/aos@next/dist/aos.js" async></script>
             </Head>
             <body>
