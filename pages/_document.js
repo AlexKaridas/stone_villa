c04a094b2f88ba29b2bc651c43c999072877a8ca
Alex Karidas <72619747+AlexKaridas@users.noreply.github.com>
2022-12-03 13:47:18 +0200

    2.2

--- pages/_document.js
+++ pages/_document.js
@@ -27,9 +27,6 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" ></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin ></link>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
-                <style>
-                    @import url(&apos;https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap&apos;);
-                </style>
                 <script src="https://unpkg.com/aos@next/dist/aos.js" async></script>
             </Head>
             <body>
